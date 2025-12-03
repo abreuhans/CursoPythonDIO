7f8200ac47e50efc5482158e3ba6636e6c01d69a
Hans Vogel <abreuhans@gmail.com>
2025-12-03 20:51:00 -0300

    Create variaveis_constantes.py

---
+++ variaveis_constantes.py
@@ -0,0 +1,14 @@
+nome = 'Guilherme'
+idade = 30
+
+nome, idade = 'Ana', 25
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZIL_STATES = ['SP', 'RJ', 'MG', 'ES']
+
+BRAZIL_STATES = ['RS', 'SC', 'PR']
+
+print(BRAZIL_STATES)

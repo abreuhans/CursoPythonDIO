9369deca778ad07528b6848912591b7c506400d2
Hans Vogel <abreuhans@gmail.com>
2025-12-03 21:14:29 -0300

    Create conversão.py

---
+++ conversão.py
@@ -0,0 +1,8 @@
+print(int("42"))          # Converte string para inteiro
+print(float("3.14"))        # Converte string para float
+print(str(100))            # Converte inteiro para string
+print(bool(1))             # Converte inteiro para booleano (True)
+
+valor = 10
+valor_str = str(valor)    # Converte inteiro para string
+print(type(valor_str))  # Verifica o tipo da variável convertida
